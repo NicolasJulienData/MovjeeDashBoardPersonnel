1b5da56f15184291f5d99b1cd102922d7146eb05
NicolasJulienData <128190647+NicolasJulienData@users.noreply.github.com>
2023-03-27 11:49:36 +0200

    Update Dashboard_Moovjee.py

--- Dashboard_Moovjee.py
+++ Dashboard_Moovjee.py
@@ -42,9 +42,8 @@ from io import StringIO
 import pandas as pd
 import requests
 
-req = Request('http://www.cmegroup.com/', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'})
-data_test = urlopen(req).read()
-pd.read_csv(data_test)
+storage_options = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
+pd.read_csv(path, storage_options=storage_options))
 data = pd.read_csv(path)
 
 today = datetime.now().strftime("%m-%d")
