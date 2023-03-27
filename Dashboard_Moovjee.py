17b5d96cc28332ee5a9721a9bff55e5f6433f56c
NicolasJulienData <128190647+NicolasJulienData@users.noreply.github.com>
2023-03-27 11:42:56 +0200

    Update Dashboard_Moovjee.py

--- Dashboard_Moovjee.py
+++ Dashboard_Moovjee.py
@@ -44,7 +44,7 @@ import requests
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
 content=requests.get(path, headers= headers)
-data_test = urlopen(req,timeout=10).read()
+data_test = urlopen(content,timeout=10).read()
 pd.read_csv(data_test)
 data = pd.read_csv(path)
 
