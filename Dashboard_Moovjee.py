5c75d44cd6b98a7a2f27ac5d6bcd4ec772c31b6a
NicolasJulienData <128190647+NicolasJulienData@users.noreply.github.com>
2023-03-28 23:51:22 +0200

    Update Dashboard_Moovjee.py

--- Dashboard_Moovjee.py
+++ Dashboard_Moovjee.py
@@ -103,9 +103,8 @@ histo_classement = []
 for date in data['date'].unique():
     data_jour = data[data['date'] == date]
     classement_jour = data_jour.sort_values(by=['likes'], ascending = False)[['title', 'likes','views']].reset_index()
-    st.write(classement_jour[classement_jour['title']==projet])
-    if classement_jour[classement_jour['title']==projet] != None:
-        histo_classement.append(int(classement_jour[classement_jour['title']==projet].index[0]))
+    st.write(classement_jour[classement_jour['title']==projet].index)
+    histo_classement.append(int(classement_jour[classement_jour['title']==projet].index[0]))
 top_15_likes = int(data_today.sort_values(by=['likes'], ascending = False).iloc[14]['likes'])
              
 col8, col9 = st.columns([1,3])
